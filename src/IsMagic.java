22539c4d9bebda5ab1f89f85f68042cc8eb576ac
Nirikshamanoj <niriksha.m1709@gmail.com>
2023-07-20 12:49:13 +0530

    First commit for Is Magic

--- src/IsMagic.java
+++ src/IsMagic.java
@@ -2,20 +2,14 @@ import java.util.Scanner;
 public class IsMagic {
     static int findMagicNumber(int n)
     {
-        int sum=0;
-        while(n>0)
-        {
-            sum+=n%10;
-            n/=10;
-        }
-        return sum;
+        if(n<9)
+            return n;
+        return findMagicNumber((n/10)+(n%10));
     }
     public static void main(String[] args) {
         Scanner scanner=new Scanner(System.in);
         int n=scanner.nextInt();
-        while(n>=10) {
-          n=findMagicNumber(n);
-        }
+        n=findMagicNumber(n);
         if(n==1)
             System.out.println(1);
         else
